3c486fc1db3e023ec22e1760425fdd016dfe54ab
David Adelwine Adumolga <56456782+David-Adelwine@users.noreply.github.com>
2023-05-28 23:19:15 +0000

    Navigation Updates

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,6 +7,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../App.css'
 import { Link } from 'react-router-dom';
+import suggestion from '../Assets/suggestions/icon-suggestions.svg'
+
+
 
 
 
@@ -16,15 +19,15 @@ const Navigation = () => {
 
 <Navbar  expand="sm" className='my-3  Nav--Postion' >
       <Container >
-        <Navbar.Brand href="#">0 suggestions</Navbar.Brand>
+        <Navbar.Brand href="#" className='text-white Navigation--text'><img src={suggestion} alt='icon'/>  0 suggestions</Navbar.Brand>
   
           <Nav 
             className="me-auto my-2 my-lg-0 d-flex Move--Links "
             style={{ maxHeight: '1px' }} >
-            <Nav.Link href="#action2" >Sortby:</Nav.Link>
+            <Nav.Link href="#action2" className='text-white Navigation--text' >Sortby:</Nav.Link>
             <NavDropdown className='Nav--card' title="Most Upvotes" id="navbarScrollingDropdown">
               <Link to='/Roamap' >
-              <NavDropdown.Item  >Most Upvotes</NavDropdown.Item>
+              <NavDropdown.Item >Most Upvotes</NavDropdown.Item>
               </Link>
               <NavDropdown.Divider />
               <NavDropdown.Item >
