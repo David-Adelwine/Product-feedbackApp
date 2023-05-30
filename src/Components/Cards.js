4c03abe1c05d76f5981402b835a06bfd0fe57e2d
David Adelwine Adumolga <56456782+David-Adelwine@users.noreply.github.com>
2023-05-30 15:13:02 +0000

    Add files via upload

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import "../App.css"
+import arrowUp from '../Assets/shared/icon-arrow-up.svg';
 
 const Cards = (props) => {
   return (
     <div className='Card--elements'>
+      <section className='inner--card'>
       <h3 className='card--header'>{props.title}</h3>
       <p className='card--description'>{props.description}</p>
       <span className='faintbg--header'>{props.category}</span>
-      <p className='card--votes'>{props.upvotes}</p>
+      <div className='card--votes faintbg'>
+        <img src={arrowUp}/>
+      <span >{props.upvotes}</span>
+      </div>
+      </section>
     </div>
   )
 }
